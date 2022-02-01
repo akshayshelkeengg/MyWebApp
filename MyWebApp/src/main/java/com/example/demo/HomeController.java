ff1b5772727172cffdc37facae40e08d1d13e59e
akshayshelkeengg <akshayshelkeengg@gmail.com>
2022-02-01 17:43:07 +0530

    STS

---
+++ MyWebApp/src/main/java/com/example/demo/HomeController.java
@@ -0,0 +1,25 @@
+package com.example.demo;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class HomeController {
+	
+	@RequestMapping("hello")
+	public ModelAndView hello(Alien alien )
+	{
+		ModelAndView mv= new ModelAndView();
+		mv.addObject("obj", alien);
+		mv.setViewName("hello");
+		return mv;
+	}
+
+}
